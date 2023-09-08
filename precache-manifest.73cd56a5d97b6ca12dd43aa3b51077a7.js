b5bf58d1276b6ea78692fb26ada6be4cbf0b2e5e
setcy <setcy@users.noreply.github.com>
2023-09-08 04:04:14 +0000

    deploy: d70634a60798798d277904135db7ee13a8c53353

---
+++ precache-manifest.73cd56a5d97b6ca12dd43aa3b51077a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "defe27dead1313a864f2a4af862df129",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec3c8914f202451f5a70",
+    "url": "./static/css/main.3a147abf.chunk.css"
+  },
+  {
+    "revision": "e18d0018bf0c4aa5d281",
+    "url": "./static/js/2.2e6df2a2.chunk.js"
+  },
+  {
+    "revision": "ec3c8914f202451f5a70",
+    "url": "./static/js/main.dcf7b17d.chunk.js"
+  },
+  {
+    "revision": "d7d44927db013b78615d",
+    "url": "./static/js/runtime~main.39a20c3b.js"
+  },
+  {
+    "revision": "837e99ea750ab1684ed2661cef36cc55",
+    "url": "./static/media/bg.837e99ea.png"
+  }
+]);
